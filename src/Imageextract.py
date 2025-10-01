64e75ae1f6f74028888d012bdb0a3216564da13c
DefNotIrf <125118645+DefNotIrf@users.noreply.github.com>
2025-10-01 12:02:40 +0800

    Update Imageextract.py

--- src/Imageextract.py
+++ src/Imageextract.py
@@ -1,9 +1,9 @@
 from pdf2image import convert_from_path
 import os
 
-pdf_path = r'C:\Users\irfan.rosdin\OneDrive - Daythree Digital Berhad\Desktop\Side task\ImageText\data\kb_samples\Servicing Process L1 & L2 [CD] - Home Fibre.pdf'  # Update with your PDF filename
-output_folder = r'C:\Users\irfan.rosdin\OneDrive - Daythree Digital Berhad\Desktop\Side task\ImageText\data\images\v1'
-poppler_path = r'C:\Users\irfan.rosdin\Downloads\Release-25.07.0-0\poppler-25.07.0\Library\bin'  # <-- Use the bin folder, no quotes
+pdf_path = r'test.pdf'  # Update with your PDF filename
+output_folder = r'C:\Users\data\images\v1'
+poppler_path = r'C:\Users\bin'  # <-- Use the bin folder, no quotes
 
 os.makedirs(output_folder, exist_ok=True)
 
